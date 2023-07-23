db8d5d94d9f5a6c66fd6d598c9ea3b2f9278e757
ugurukku <ugurkarimli1@gmail.com>
2023-07-24 03:15:39 +0400

    some changes

--- src/main/java/com/ugurukku/kitabal/entities/Publisher.java
+++ src/main/java/com/ugurukku/kitabal/entities/Publisher.java
@@ -5,6 +5,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 @Data
 @Builder
