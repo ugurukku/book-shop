ec4b9c8f7a454aa226cad4d1a4f2564816dcc4cf
ugurukku <ugurkarimli1@gmail.com>
2023-07-26 02:47:31 +0400

    optimizing cart model

---
+++ src/main/java/com/ugurukku/kitabal/dto/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.ugurukku.kitabal.dto.mapper;
+
+import com.ugurukku.kitabal.dto.UserRequest;
+import com.ugurukku.kitabal.entities.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toEntity(UserRequest userRequest);
+}
