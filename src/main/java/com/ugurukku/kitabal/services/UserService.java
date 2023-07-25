2dbaa07e4b1a57116c437173a6b154bf2a06b271
ugurukku <ugurkarimli1@gmail.com>
2023-07-26 02:47:35 +0400

    optimizing cart model

---
+++ src/main/java/com/ugurukku/kitabal/services/UserService.java
@@ -0,0 +1,7 @@
+package com.ugurukku.kitabal.services;
+
+import com.ugurukku.kitabal.dto.UserRequest;
+
+public interface UserService {
+    void add(UserRequest userRequest);
+}
---
+++ src/main/java/com/ugurukku/kitabal/services/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ugurukku.kitabal.services.impl;
+
+import com.ugurukku.kitabal.dto.mapper.UserMapper;
+import com.ugurukku.kitabal.dto.UserRequest;
+import com.ugurukku.kitabal.entities.SequenceGeneratorService;
+import com.ugurukku.kitabal.entities.User;
+import com.ugurukku.kitabal.repositories.UserRepository;
+import com.ugurukku.kitabal.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    private final UserMapper mapper;
+
+    private final SequenceGeneratorService sequenceGenerator;
+
+    @Override
+    public void add(UserRequest userRequest) {
+        User user = mapper.toEntity(userRequest);
+        user.setId(sequenceGenerator.generateSequence(User.SEQUENCE_NAME));
+
+        repository.save(user);
+    }
+}
