45c1cb6c1d15e0ca54763d02a14072754a027c26
ugurukku <ugurkarimli1@gmail.com>
2023-07-27 16:33:45 +0400

    user login logic

--- src/main/java/com/ugurukku/kitabal/dto/auth/LoginResponse.java
+++ src/main/java/com/ugurukku/kitabal/dto/auth/LoginResponse.java
@@ -1,4 +1,4 @@
-package com.ugurukku.kitabal.dto;
+package com.ugurukku.kitabal.dto.auth;
 
 
 import lombok.AllArgsConstructor;
--- src/main/java/com/ugurukku/kitabal/entities/User.java
+++ src/main/java/com/ugurukku/kitabal/entities/User.java
@@ -35,6 +35,8 @@ public class User {
 
     String password;
 
+    Boolean isActive;
+
     Role role;
 
 }
