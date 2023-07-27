1e784be1a06af84637f1c7a55c1a990e9f4126b2
ugurukku <ugurkarimli1@gmail.com>
2023-07-27 16:20:52 +0400

    in register request password will be encoded

--- src/main/java/com/ugurukku/kitabal/services/impl/UserServiceImpl.java
+++ src/main/java/com/ugurukku/kitabal/services/impl/UserServiceImpl.java
@@ -2,10 +2,13 @@ package com.ugurukku.kitabal.services.impl;
 
 import com.ugurukku.kitabal.dto.mapper.UserMapper;
 import com.ugurukku.kitabal.dto.UserRequest;
+import com.ugurukku.kitabal.entities.Role;
 import com.ugurukku.kitabal.entities.User;
 import com.ugurukku.kitabal.repositories.UserRepository;
 import com.ugurukku.kitabal.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +21,21 @@ public class UserServiceImpl implements UserService {
 
     private final SequenceGeneratorService sequenceGenerator;
 
+    private final PasswordEncoder encoder;
+
     @Override
     public void add(UserRequest userRequest) {
         User user = mapper.toEntity(userRequest);
+        user.setPassword(encoder.encode(user.getPassword() ));
         user.setId(sequenceGenerator.generateSequence(User.SEQUENCE_NAME));
+        user.setRole(Role.USER);
+        user.setIsActive(true);
 
         repository.save(user);
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return repository.findUserByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Istifadəçi adı və yta şifrə yanlışdır"));
     }
 }
